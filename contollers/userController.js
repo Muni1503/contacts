27eafdb09d5822f1b41324bab93d501eb2f6f49a
Muni1503 <rathinaraomuni@gmail.com>
2023-09-07 19:40:45 +0530

    adding body to display for logged in user

--- contollers/userController.js
+++ contollers/userController.js
@@ -64,7 +64,7 @@ const loginUser=asyncHandler(async (req,res)=>{
 }) ;
 
 const currentUser=asyncHandler(async (req,res)=>{
-    res.json({message:"current user"});
+    res.json(req.user);
 }) ;
 
 module.exports={registerUser,loginUser,currentUser};
